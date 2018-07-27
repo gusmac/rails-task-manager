83c08a442efdd4b6228e553c5af8d91733cc8056
Gus Macfarlane <gusmac@gmail.com>
2018-07-27 15:57:46 +0800

    all done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,6 +20,17 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     @task.update(task_params)
+    redirect_to task_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
 
   get "tasks/:id", to: "tasks#show", as: :task
 
-  get    "new",      to: "tasks#new"
-  post   "tasks",          to: "tasks#create", as: :create
+  get "new", to: "tasks#new"
+  post "tasks", to: "tasks#create", as: :create
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update", as: :update
+
+  delete "tasks/:id", to: "tasks#destroy", as: :delete
 end
